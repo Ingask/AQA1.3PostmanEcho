7bf74d7177dfa3f8aa613c71482a06f5fd6b6df8
Inga Oberemchuk <inga_oberemchuk@mail.ru>
2021-01-27 00:13:56 +0300

    Initial project Postman Echo

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello"))
+        ;
+    }
+}
+
+
